77e8aee693a0a7688036b2bc23e575a2e55cf035
Ana Gauna <amgauna@gmail.com>
2022-02-07 19:14:14 -0300

    Update views.py

--- views.py
+++ views.py
@@ -4,6 +4,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def home(request):
     return HttpResponse('Hello World')
   
+# Vamos inserir as funções relativas as rotas acima no arquivo views:
+# Formulário de cadastro de usuários
+
+def create(request):
+    return render(request,'create.html')
+
+
+#Inserção dos dados dos usuários no banco
+
+def store(request):
+    data = {}
+    if(request.POST['password'] != request.POST['password-conf']):
+        data['msg'] = 'Senha e confirmação de senha diferentes!'
+        data['class'] = 'alert-danger'
+    else:
+        user = User.objects.create_user(request.POST['user'], request.POST['email'], request.POST['password'])
+        user.first_name = request.POST['name']
+        user.save()
+        data['msg'] = 'Usuário cadastrado com sucesso!'
+        data['class'] = 'alert-success'
+    return render(request,'create.html',data)
+
